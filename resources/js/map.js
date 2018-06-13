a2f8cd08916e3d379e8cef4e206d6d0c086accd9
Egglestar <svetter82@yahoo.de>
2018-06-13 23:00:06 +0200

    grid for IE adapted

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,6 +1,40 @@
 // Map aufrufen
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
 function initMap() {} // now it IS a function and it is in global
+
+
+function initMap() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    center: chicago
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+
+
+/* function initMap() {} // now it IS a function and it is in global
 
   initMap = function() {
     var giuliano = {lat: 39.84982, lng: 18.336944};
@@ -14,9 +48,10 @@ function initMap() {} // now it IS a function and it is in global
             map: map
           });
 
+    //
       // info der der karte zufügen
-    /*var infoWindow = new google.maps.InfoWindow({map: map});
+    var infoWindow = new google.maps.InfoWindow({map: map});
             infoWindow.setPosition(giuliano);
             infoWindow.setContent('Masseria Prontera in Giuliano di Lecce');
-            map.setCenter(giuliano);*/
-  };
+            map.setCenter(giuliano);
+  }; */
